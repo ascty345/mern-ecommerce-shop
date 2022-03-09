8990ef34238a52a98ced67556a0dfc1422365c27
tue <tuedinh28@gmail.com>
2022-03-09 18:48:54 +0700

    show User In Navbar & create Logout Functionality

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 // import { LinkContainer } from 'react-router-bootstrap'
 import { Link } from 'react-router-dom'
-import { Container, Navbar, Nav } from 'react-bootstrap'
+import { Container, Navbar, Nav, NavDropdown } from 'react-bootstrap'
+import { logout } from '../actions/userActions'
 
 const Header = () => {
+  const dispatch = useDispatch()
+
+  const userLogin = useSelector((state) => state.userLogin)
+  const { userInfo } = userLogin
+
+  const logoutHandler = () => {
+    dispatch(logout())
+  }
+
   return (
     <header>
       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
@@ -12,14 +23,27 @@ const Header = () => {
             ProShop
           </Navbar.Brand>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
-          <Navbar.Collapse id='basic-navbar-nav'>
-            <Nav className='me-auto'>
+          <Navbar.Collapse
+            className='justify-content-end'
+            id='basic-navbar-nav'>
+            <Nav>
               <Nav.Link as={Link} to='/cart'>
                 <i className='fas fa-shopping-cart'></i> Cart
               </Nav.Link>
-              <Nav.Link as={Link} to='/login'>
-                <i className='fas fa-user'></i> Sign In
-              </Nav.Link>
+              {userInfo ? (
+                <NavDropdown title={userInfo.name} id='username'>
+                  <NavDropdown.Item as={Link} to='/profile'>
+                    Profile
+                  </NavDropdown.Item>
+                  <NavDropdown.Item onClick={logoutHandler}>
+                    Logout
+                  </NavDropdown.Item>
+                </NavDropdown>
+              ) : (
+                <Nav.Link as={Link} to='/login'>
+                  <i className='fas fa-user'></i> Sign In
+                </Nav.Link>
+              )}
             </Nav>
           </Navbar.Collapse>
         </Container>
