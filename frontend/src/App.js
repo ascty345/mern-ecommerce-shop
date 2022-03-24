771f2f74dc0fb53e594d6e195a1a80387eb31cba
tue <tuedinh28@gmail.com>
2022-03-24 18:55:49 +0700

    edit product screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,6 +15,7 @@ import OrderScreen from './screens/OrderScreen'
 import UserListScreen from './screens/UserListScreen'
 import UserEditScreen from './screens/UserEditScreen'
 import ProductListScreen from './screens/ProductListScreen'
+import ProductEditScreen from './screens/ProductEditScreen'
 
 const App = () => {
   return (
@@ -36,6 +37,10 @@ const App = () => {
             <Route path='/admin/userList' element={<UserListScreen />} />
             <Route path='/admin/user/:id/edit' element={<UserEditScreen />} />
             <Route path='/admin/productList' element={<ProductListScreen />} />
+            <Route
+              path='/admin/product/:id/edit'
+              element={<ProductEditScreen />}
+            />
             <Route path='/' element={<HomeScreen />} />
           </Routes>
         </Container>
