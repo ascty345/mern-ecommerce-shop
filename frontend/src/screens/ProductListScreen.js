11523fb5661df8deb96d4c19fa08eed59df3f668
tue <tuedinh28@gmail.com>
2022-03-24 13:58:33 +0700

    admin product list

---
+++ frontend/src/screens/ProductListScreen.js
@@ -0,0 +1,98 @@
+import React, { useEffect } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { Table, Button, Row, Col } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import Message from '../components/Message'
+import Loader from '../components/Loader'
+import { listProduct } from '../actions/productActions'
+
+const ProductListScreen = () => {
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  const productList = useSelector((state) => state.productList)
+  const { loading, error, products } = productList
+
+  const userLogin = useSelector((state) => state.userLogin)
+  const { userInfo } = userLogin
+
+  useEffect(() => {
+    if (userInfo && userInfo.isAdmin) {
+      dispatch(listProduct())
+    } else {
+      navigate('/login')
+    }
+  }, [dispatch, userInfo, navigate])
+
+  const deleteHandler = (id) => {
+    if (window.confirm('Are you sure')) {
+      // DELETE PRODUCTS
+    }
+  }
+
+  const createProductHandler = (product) => {
+    console.log('create a product')
+  }
+
+  return (
+    <>
+      <Row>
+        <Col>
+          <h1>Products</h1>
+        </Col>
+        <Col className='d-flex justify-content-end'>
+          <Button className='my-3' onClick={createProductHandler}>
+            <i className='fas fa-plus' />
+            Create Product
+          </Button>
+        </Col>
+      </Row>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant='danger'>{error}</Message>
+      ) : (
+        <Table striped bordered hover responsive className='table-sm'>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>NAME</th>
+              <th>PRICE</th>
+              <th>CATEGORY</th>
+              <th>BRAND</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {products.map((product) => (
+              <tr key={product._id}>
+                <td>{product._id}</td>
+                <td>{product.name}</td>
+                <td>${product.price}</td>
+                <td>{product.category}</td>
+                <td>{product.brand}</td>
+                <td>
+                  <Button
+                    as={Link}
+                    to={`/admin/product/${product._id}/edit`}
+                    className='btn-sm'
+                    variant='light'>
+                    <i className='fas fa-edit' />
+                  </Button>
+                  <Button
+                    variant='danger'
+                    className='btn-sm'
+                    onClick={deleteHandler.bind(null, product._id)}>
+                    <i className='fas fa-trash' />
+                  </Button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      )}
+    </>
+  )
+}
+
+export default ProductListScreen
